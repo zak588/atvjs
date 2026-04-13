39ed9ce1c8f4b5c80beea4619bf2f8fbf5f6288a
gutãozak <gutorw2000@gmail.com>
2026-04-13 12:20:11 -0300

    Add files via upload

---
+++ trabalho tds/Exercicio2.js
@@ -0,0 +1,10 @@
+let vida = 100;
+let Ataque = 30;
+
+let dano = vida - Ataque;
+
+if(Ataque >= vida){
+    console.log("Game Over Ha Ha Ha!!")
+}else{
+    console.log("Você continua vivo")
+}
---
+++ trabalho tds/Exercicio3.js
@@ -0,0 +1,6 @@
+let distancia = 300;
+let consumo = 25;
+
+let conumoMedio = (distancia/consumo);
+
+console.log("O consumo médio de combustível foi de",conumoMedio,"KM por litro");
