3094c6bdcdc5b1bf6df6c525437bea593a07df59
gutãozak <gutorw2000@gmail.com>
2026-04-15 08:17:45 -0300

    Add files via upload

---
+++ trabalho tds/JS/Exercicio4.js
@@ -0,0 +1,13 @@
+let nota1 = 6
+let nota2 = 7
+let nota3 = 8
+let nota4 = 5
+
+let media = (nota1 + nota2 + nota3 + nota4)/4
+if (media >= 7){
+    console.log ("Você esta aprovado");
+}else if(media >= 5){
+    console.log ("Voce esta de recuperação")
+}else{
+    console.log ("Voce esta reprovado")
+}
