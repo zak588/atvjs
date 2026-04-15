60d4f8b4b62399d1519cf47396f2e427b5524de2
gutãozak <gutorw2000@gmail.com>
2026-04-15 08:19:26 -0300

    Add files via upload

---
+++ JS/Exercicio1.js
@@ -0,0 +1,10 @@
+let filme1 = 120
+let filme2 = 95
+let filme3 = 110
+let soma = (filme1 + filme2 + filme3)+3;
+
+console. log("Tempo Assistido: ", soma);
+
+let media = (filme1 + filme2 + filme3)/3;
+let mediafixed = Math.round(media);
+console. log("Média de duração dos filmes é de",mediafixed, "minutos");
---
+++ JS/Exercicio5.js
@@ -0,0 +1,13 @@
+let partida1 = 2
+let partida2 = 1
+let partida3 = 3
+let partida4 = 0
+let partida5 = 4
+
+let soma = (partida1 + partida2 + partida3 + partida4 + partida5);
+
+console.log("A soma dos gols foi de",soma,"no total");
+
+let media = (partida1 + partida2 + partida3 + partida4 + partida5)/5;
+
+console.log("A media dos gols total foi de",media,"gols");
