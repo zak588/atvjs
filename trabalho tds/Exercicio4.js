122f89f36bb40f8b530ff3f6f9b9fb40faf3f7ec
gutãozak <gutorw2000@gmail.com>
2026-04-15 08:19:05 -0300

    Delete trabalho tds directory

--- trabalho tds/Exercicio4.js
+++
@@ -1,6 +0,0 @@
-let nota1 = 6
-let nota2 = 7
-let nota3 = 8
-let nota4 = 5
-
-let media = (nota1 + nota2 + nota3 + nota4)/4
--- trabalho tds/JS/Exercicio3.js
+++
@@ -1,6 +0,0 @@
-let distancia = 300;
-let consumo = 25;
-
-let conumoMedio = (distancia/consumo);
-
-console.log("O consumo médio de combustível foi de",conumoMedio,"KM por litro");
